da2157c8e7f633f42f9733f154ee168db0f81e16
Gautam <164214553+Creator51@users.noreply.github.com>
2025-08-25 20:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (80.51%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // Helper function: count subarrays with sum <= goal
     int f(vector<int>& nums, int goal) {
-        if (goal < 0) return 0; // no valid subarray
+        if (goal < 0) return 0; 
         int l = 0, r = 0;
         int n = nums.size();
         int s = 0, ans = 0;
