ee2f6a0b31f8ad09f514c394c7e27716273cda81
Gautam <164214553+Creator51@users.noreply.github.com>
2025-09-04 10:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (45.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,9 +3,11 @@ public:
     bool check(vector<int>& nums) {
         int drop=0;
         for(int i=1;i<nums.size();i++){
+            //THe Main Logic is Drop should be <= 1 
             if(nums[i]<nums[i-1])
-            drop++;
+            drop++; //Sorted Array Contains Only 1 Drop
         }
+        //If the Array is Not Correctly there will be an Drop
         if(nums[nums.size()-1]>nums[0])
         drop++;
 
