4717622e10d645bdcd5f5b94af5be2caa0f5eb70
Gautam <164214553+Creator51@users.noreply.github.com>
2025-09-11 05:22:22 +0530

    Time: 1754 ms (5.00%) | Memory: 474.4 MB (5.00%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         set<vector<int>> st;
-        int n = nums.size();
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                //set<int> hashset;
+                unordered_set<long long> hashset; 
+                for(int k=j+1;k<nums.size();k++){
+                    long long sum = (long long)nums[i] + nums[j] + nums[k]; // ✅ safer cast
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                unordered_set<long long> hashset;
-                for(int k = j + 1; k < n; k++) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k];
-                    long long fourth = (long long)target - sum;
-
-                    if(hashset.find(fourth) != hashset.end()) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
+                    // long long fourth = target - sum;
+                    long long fourth = (long long)target - sum; // ✅ consistent long long
+                    if(hashset.find(fourth)!=hashset.end()){
+                        vector<int> temp={nums[i],nums[j],nums[k],(int)fourth};
+                        sort(temp.begin(),temp.end());
                         st.insert(temp);
                     }
-                    hashset.insert(nums[k]);
+                       hashset.insert(nums[k]); 
                 }
             }
         }
 
-        return vector<vector<int>>(st.begin(), st.end());
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+        
     }
 };
