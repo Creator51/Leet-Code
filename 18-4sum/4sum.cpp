0e92388f773a80bc0d4683c422994d0e7a626f33
Gautam <164214553+Creator51@users.noreply.github.com>
2025-09-11 05:44:08 +0530

    Time: 23 ms (55.05%) | Memory: 17.5 MB (27.31%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> st;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                set<long long> hashset;
-                for(int k=j+1;k<nums.size();k++){
-                    long long sum = (long long)nums[i] + nums[j] + nums[k]; // ✅ safer cast
-
-                    // long long fourth = target - sum;
-                    long long fourth = (long long)target - sum; // ✅ consistent long long
-                    if(hashset.find(fourth)!=hashset.end()){
-                        vector<int> temp={nums[i],nums[j],nums[k],(int)fourth};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                    }
-                       hashset.insert(nums[k]); 
-                }
+       int n=nums.size();
+       vector<vector<int>> ans;
+       // We will proceed with this Answer just Like the 3 Sum first we will sort the Array
+       //We are USing 4 Pointer Approach(i,j,k,l)
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<n;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue; //To skip the Dublicates of I
+        for(int j=i+1;j<n;j++){
+            if(j != i+1 && nums[j]==nums[j-1]) continue; // TO skip the Dublicates of J 
+            int k=j+1;
+            int l=n-1;
+            while(k<l){
+                long long sum=nums[i];
+                sum+=nums[j];
+                sum+=nums[k];
+                sum+=nums[l];
+                if(sum==target){
+                    vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                    ans.push_back(temp);
+                    k++,l--;
+                    while(k < l && nums[k]==nums[k-1])k++;
+                    while(k < l && nums[l]==nums[l+1])l--;
+                    
+                }else if(target > sum) 
+                k++;
+                else
+                l--;
             }
+            
         }
-
-        vector<vector<int>> ans(st.begin(),st.end());
-        return ans;
+       }
+       return ans;
         
     }
 };
