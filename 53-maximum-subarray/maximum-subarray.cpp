9c8d3736c014d988db738c5115d0c58288867441
Gautam <164214553+Creator51@users.noreply.github.com>
2025-09-17 03:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (16.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,13 +3,14 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
         int max=INT_MIN;
+        //Kadanes Algorithm
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
 
             if(sum>max)
             max=sum;
 
-
+            //If the Sum is going <0 Reset it as it has No Point
             if(sum<0)
             sum=0;
 
