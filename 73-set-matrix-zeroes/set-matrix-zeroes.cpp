15e1a3232c79bada9d4be42ce8a68e6467e483fb
Gautam <164214553+Creator51@users.noreply.github.com>
2025-09-24 11:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,6 +4,10 @@ public:
         vector<int> rows(matrix.size(), 0);
         vector<int> cols(matrix[0].size(), 0);
 
+        //So Basically we are going to Locate the 0 in the matrix and SToring their Coloum
+        // and rown so that In the next Iteration if rows || cols == 1 the full row and Coloum 
+        // Becomes 0
+
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
@@ -19,7 +23,7 @@ public:
 
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++)
-{
+            {
                 if(rows[i]==1 || cols[j]==1){
                     matrix[i][j]=0;
                 }
