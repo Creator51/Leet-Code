122bf2cb100e7d9e74c83f7a0a5a7e00cf6d00f0
Gautam <164214553+Creator51@users.noreply.github.com>
2026-03-22 13:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (85.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -3,18 +3,17 @@ class Solution:
         hash_num1={}
         for i in range(len(nums1)):
             hash_num1[nums1[i]]=i
-        
-        stack=[]
         res = [-1] * len(nums1)
+        stack=[]
         for i in range(len(nums2)):
-            cur = nums2[i]
-            while stack and cur > stack[-1]:
+            curr = nums2[i]
+            while stack and curr> stack[-1]:
                 val = stack.pop()
-                Index=hash_num1[val]
-                res[Index] = cur
+                Index = hash_num1[val]
+                res[Index]=curr
 
-            if cur in hash_num1:
-                stack.append(cur)
+            if curr in hash_num1:
+                stack.append(curr)
+
         return res
-
         
