bb4489364d013717d09a181926fd9690c13441a0
Gautam <164214553+Creator51@users.noreply.github.com>
2026-03-26 11:45:21 +0530

    Time: 41 ms (33.84%) | Memory: 42.7 MB (50.33%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,17 +1,31 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr=[]
-        temp = head
-        while temp:
-            arr.append(temp.val)
-            temp=temp.next
-        if arr==arr[::-1]:
-            return True
-        else:
-            return False
-        
+        # Step 1: find middle
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: reverse second half
+        prev = None
+        curr = slow
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        # Step 3: compare both halves
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
