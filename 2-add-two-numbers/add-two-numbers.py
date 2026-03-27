33fd791096fee4736003fa04a124fe8329570967
Gautam <164214553+Creator51@users.noreply.github.com>
2026-03-27 13:15:55 +0530

    Time: 7 ms (24.89%) | Memory: 19.3 MB (89.17%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,32 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        arr1, arr2 = [], []
-        t1, t2 = l1, l2
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 
-        while t1:
-            arr1.append(t1.val)
-            t1 = t1.next
-
-        while t2:
-            arr2.append(t2.val)
-            t2 = t2.next
-
-        arr1 = arr1[::-1]
-        arr2 = arr2[::-1]
-
-        dummy = ListNode(0)
+        dummy = ListNode()
         curr = dummy
-        carry = 0
+        carry = 0 
 
-        while arr1 or arr2 or carry:
-            x = arr1.pop() if arr1 else 0
-            y = arr2.pop() if arr2 else 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2= l2.val if l2 else 0
 
-            total = x + y + carry
-            carry = total // 10
+            val = v1 + v2 + carry
 
-            node = ListNode(total % 10)
-            curr.next = node
-            curr = curr.next
+            carry = val // 10 
+            val = val % 10
+            curr.next = ListNode(val)
+            curr=curr.next
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
 
         return dummy.next
+
+        
