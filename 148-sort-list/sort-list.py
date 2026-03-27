6febf2a34c5d7081ea122e52a7ce8c9b69fb9f97
Gautam <164214553+Creator51@users.noreply.github.com>
2026-03-27 11:54:11 +0530

    Time: 189 ms (30.30%) | Memory: 40.6 MB (94.49%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -5,20 +5,49 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr=[]
-
-        temp=head
-        while temp:
-            arr.append(temp.val)
-            temp=temp.next
-        i=0
-        arr.sort()
-        temp=head
-        while temp:
-            temp.val=arr[i]
-            temp=temp.next
-            i+=1
-
-        return head
-
         
+        # Now doing it without extra Space and Reursive
+
+        if not head or not head.next:
+            return head
+
+        #Split the list into 2 halfes Left and right
+
+        left = head
+        right = self.getmiddle(head)
+        temp=right.next
+        right.next=None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left,right)
+
+    def getmiddle(self,head):
+        slow , fast = head , head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def merge(self,left,right):
+        dummy = ListNode(0)
+        curr=dummy
+        while left and right:
+            if left.val < right.val:
+                curr.next=left
+                left=left.next
+            else:
+                curr.next = right
+                right = right.next
+
+            curr = curr.next
+
+        if left :
+            curr.next = left
+        else:
+            curr.next = right
+
+        return dummy.next
