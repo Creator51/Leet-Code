a196d755a1a0e7d09f50cb7a9f8c26897d3634ac
Gautam <164214553+Creator51@users.noreply.github.com>
2026-04-19 06:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (97.94%) - LeetSync

---
+++ 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def maximumTop(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+
+        if k ==0:
+            return nums[0]
+
+        if n ==1:
+            return nums[0] if k%2==0 else -1
+
+        if k == 1 :
+            return nums[1]
+
+        if k > n:
+            return max(nums)
+
+        if k == n:
+            return max(nums[:k-1])
+
+        return max(max(nums[:k-1]),nums[k])
+            
+
+
+        
