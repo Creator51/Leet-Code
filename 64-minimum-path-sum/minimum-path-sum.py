e9e3e5404c0e3847cc4f5b7ba9280fcb6ed04c31
Gautam <164214553+Creator51@users.noreply.github.com>
2026-06-27 13:37:58 +0530

    Time: 26 ms (22.19%) | Memory: 21.7 MB (50.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,24 +1,45 @@
 class Solution:
-    def helper(self,i,j,grid,dp):
+    # def helper(self,i,j,grid,dp):
 
-        if dp[i][j]!=-1:
-            return dp[i][j]
-            
-        if i==0 and j==0 :
-            return grid[i][j]
+    #     if dp[i][j]!=-1:
+    #         return dp[i][j]
 
-        if i<0 or j<0 :
-            return float('inf')
+    #     if i==0 and j==0 :
+    #         return grid[i][j]
 
-        up = grid[i][j] + self.helper(i-1,j,grid,dp)
-        left = grid[i][j] + self.helper(i,j-1,grid,dp)
+    #     if i<0 or j<0 :
+    #         return float('inf')
 
-        dp[i][j]= min(up,left)
-        return min(up,left)
+    #     up = grid[i][j] + self.helper(i-1,j,grid,dp)
+    #     left = grid[i][j] + self.helper(i,j-1,grid,dp)
+
+    #     dp[i][j]= min(up,left)
+    #     return min(up,left)
     def minPathSum(self, grid: List[List[int]]) -> int:
-        i=len(grid)
-        j=len(grid[0])
+        m=len(grid)
+        n=len(grid[0])
 
-        dp = [[-1] * j for _ in range(i)]
-        return self.helper(i-1,j-1,grid,dp)
+        dp = [[-1] * n for _ in range(m)]
+        # return self.helper(i-1,j-1,grid,dp)
+        #Tabulation 
+        dp[0][0]=grid[0][0]
+
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0 :
+                    dp[i][j]= grid[i][j]
+                    continue
+
+                up = float('inf')
+                left = float('inf')              
+
+                if i>0:
+                    up = grid[i][j] + dp[i-1][j]
+                if j>0:
+                    left = grid[i][j] + dp[i][j-1]
+
+                dp[i][j]=min(up,left)
+
+        return dp[m-1][n-1]
+
         
