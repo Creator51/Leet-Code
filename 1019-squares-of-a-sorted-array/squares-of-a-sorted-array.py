d2e48abc5d679510f28068c71c5018711dfe848f
Gautam <164214553+Creator51@users.noreply.github.com>
2026-07-15 14:16:23 +0530

    Time: 7 ms (78.21%) | Memory: 21.1 MB (28.48%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,11 +1,28 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums_new=[]
-        for i in nums:
-            nums_new.append(i*i)
+        # nums_new=[]
+        # for i in nums:
+        #     nums_new.append(i*i)
 
-        nums_new.sort()
+        # nums_new.sort()
         
-        return nums_new
+        # return nums_new
+        #Two pointer approach
+
+        res=[]
+
+        l,r=0,len(nums)-1
+
+        while l<=r:
+            if nums[l]*nums[l] > nums[r] * nums[r]:
+                res.append(nums[l]*nums[l])
+                l+=1
+            else:
+                res.append(nums[r]*nums[r])
+                r-=1
+
+        return res[::-1]
+
+
 
         
