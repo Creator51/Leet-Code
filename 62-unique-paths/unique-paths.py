851fd408ebd72d6510a308e283fedb80c815a001
Gautam <164214553+Creator51@users.noreply.github.com>
2026-08-01 11:13:18 +0530

    Time: 3 ms (22.13%) | Memory: 12.5 MB (8.40%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,39 +1,26 @@
-class Solution:
-    #Memorization
-    # def helper(self,i,j,dp):
+class Solution(object):
 
-    #     if dp[i][j]!= -1:
-    #         return dp[i][j]
-
-    #     if i==0 and j ==0 :
-    #         return 1
-    #     if i < 0 or j < 0:
-    #         return 0
-
-    #     up=self.helper(i-1,j,dp)
-    #     left=self.helper(i,j-1,dp)
-
-    #     dp[i][j]=up + left
-    #     return up + left
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1]*n for _ in range(m)]
-        # return self.helper(m-1,n-1,dp)
-
-        dp[0][0]=1
-
-        for i in range(m):
-            for j in range(n):
-
-                if i ==0 and j==0:
-                    continue
-
-                up=0
-                left=0
-                if i > 0:
-                    up=dp[i-1][j]
-                if j > 0:
-                    left=dp[i][j-1]
-
-                dp[i][j]= left + up
-        return dp[m-1][n-1]
+    def helper(self,m,n,dp):
         
+        if dp[m][n]!=-1:
+            return dp[m][n]
+        if m==0 and n==0:
+            return 1
+
+        if m<0 or n<0:
+            return 0
+
+        up=self.helper(m-1,n,dp)
+        left=self.helper(m,n-1,dp)
+
+        dp[m][n]=up+left
+        return up+left
+        
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[-1]*n for _ in range(m)]
+        return self.helper(m-1,n-1,dp)
