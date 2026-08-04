e6808a73a9367e5661ec4780b093d59f854c6260
Gautam <164214553+Creator51@users.noreply.github.com>
2026-08-04 16:17:29 +0530

    Time: 7 ms (77.84%) | Memory: 24.1 MB (12.09%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        x=float('-inf')
+        maxi=[x]
+        def helper(root,maxi):
+
+            if not root:
+                return 0
+
+            left=max(0,helper(root.left,maxi))
+            right=max(0,helper(root.right,maxi))
+
+            maxi[0]=max(maxi[0],root.val+left+right)
+
+            return root.val+max(left,right)
+        helper(root,maxi)
+        return maxi[0]
+        
