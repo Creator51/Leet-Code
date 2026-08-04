fe93d8f1823dc280e1abbeb509bf30727d07f974
Gautam <164214553+Creator51@users.noreply.github.com>
2026-08-04 14:15:47 +0530

    Time: 2 ms (80.76%) | Memory: 22 MB (89.11%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,30 +7,46 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
 
-        def find_height(root):
+        # def find_height(root):
+        #     if not root:
+        #         return 0
+
+        #     lf=find_height(root.left)
+        #     rt=find_height(root.right)
+
+        #     return max(lf,rt)+1
+
+        
+        # maxi=float('-inf')
+
+        # def finder(root,maxi):
+        #     if not root:
+        #         return 0
+
+        #     left_height=find_height(root.left)
+        #     right_height=find_height(root.right)
+
+        #     current=left_height+right_height
+
+        #     left=finder(root.left,maxi)
+        #     right=finder(root.right,maxi)
+
+        #     return max(current,left,right)
+        
+        # return finder(root,maxi)
+
+        #Now trying optimal
+        maxi=[0]
+        def find_height(root,maxi):
             if not root:
                 return 0
 
-            lf=find_height(root.left)
-            rt=find_height(root.right)
+            lf=find_height(root.left,maxi)
+            rt=find_height(root.right,maxi)
+
+            maxi[0]=max(maxi[0],lf+rt)
 
             return max(lf,rt)+1
 
-        
-        maxi=float('-inf')
-
-        def finder(root,maxi):
-            if not root:
-                return 0
-
-            left_height=find_height(root.left)
-            right_height=find_height(root.right)
-
-            current=left_height+right_height
-
-            left=finder(root.left,maxi)
-            right=finder(root.right,maxi)
-
-            return max(current,left,right)
-        
-        return finder(root,maxi)
+        find_height(root,maxi)
+        return maxi[0]
