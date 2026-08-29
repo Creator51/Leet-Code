af837dc750bb0b5e4830e847a76c55a2266655ce
Gautam <164214553+Creator51@users.noreply.github.com>
2026-08-29 14:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (64.00%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -1,25 +1,17 @@
-class NumArray(object):
+class NumArray:
 
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        self.nums= nums
-        for i in range(1,len(nums)):
-            self.nums[i]=nums[i-1]+nums[i]
-        
-        
+    def __init__(self, nums: List[int]):
+        self.prefix=[]
+        curr=0
+        for n in nums:
+            curr+=n
+            self.prefix.append(curr)
 
-    def sumRange(self, left, right):
-        """
-        :type left: int
-        :type right: int
-        :rtype: int
-        """
-        if left ==0 :
-            return self.nums[right]
-        return self.nums[right]-self.nums[left-1]
-        
+    def sumRange(self, left: int, right: int) -> int:
+        rightsum=self.prefix[right]
+        leftsum=self.prefix[left-1] if left > 0 else 0
+
+        return rightsum-leftsum 
         
 
 
