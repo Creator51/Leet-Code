77942180687e6c0537f47787fceeb182ac135e7d
Gautam <164214553+Creator51@users.noreply.github.com>
2026-09-08 14:39:50 +0530

    Time: 113 ms (81.49%) | Memory: 19.2 MB (68.65%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,22 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res=0
+        cnt=0
 
         for i in range(len(s)):
             left=right=i
-            #for odd
-            while left >=0 and right <len(s) and s[left]==s[right]:
-                res+=1
+
+            while left>=0 and right<len(s)  and s[left]==s[right]:
                 left-=1
                 right+=1
+                cnt+=1
 
             left,right=i,i+1
 
-            #for even 
-            while left>=0 and right < len(s) and s[left]==s[right]:
-                res+=1
+            while left>=0 and right<len(s) and s[left]==s[right]:
                 left-=1
                 right+=1
+                cnt+=1
 
-        return res
+        return cnt
+
+
         
