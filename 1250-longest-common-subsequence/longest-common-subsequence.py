c5ac92208ad1c78150cb7ec25b88f9843491a7c0
Gautam <164214553+Creator51@users.noreply.github.com>
2026-09-08 14:59:04 +0530

    Time: 603 ms (26.38%) | Memory: 44.3 MB (74.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,22 +1,23 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        ans=0
 
         dp=[[-1]*len(text2) for _ in range(len(text1))]
-        
+
         def solve(idx1,idx2):
 
-            if idx1<0 or idx2<0:
+            if idx1 <0 or idx2<0:
                 return 0
-            
+
             if dp[idx1][idx2]!=-1:
-                return dp[idx1][idx2]            
+                return dp[idx1][idx2]
 
             if text1[idx1]==text2[idx2]:
-                dp[idx1][idx2]=1 + solve(idx1-1,idx2-1)
+                dp[idx1][idx2]=1+solve(idx1-1,idx2-1)
                 return dp[idx1][idx2]
             
             dp[idx1][idx2]=max(solve(idx1-1,idx2),solve(idx1,idx2-1))
+
             return dp[idx1][idx2]
 
         return solve(len(text1)-1,len(text2)-1)
-        
